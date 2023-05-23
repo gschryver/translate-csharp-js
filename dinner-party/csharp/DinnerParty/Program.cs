986bcc7b97ff94e5ae3761bd7240dbf65e2a4300
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-05-23 16:06:36 -0400

    dinner party translation

---
+++ dinner-party/csharp/DinnerParty/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+var guests = GetGuests();
+var tables = new Dictionary<string, List<Guest>>
+{
+    {"Table 1", new List<Guest>()},
+    {"Table 2", new List<Guest>()}
+};
+
+foreach (var guest in guests)
+{
+    var table1Occupations = tables["Table 1"].Select(g => g.Occupation);
+    if (table1Occupations.Contains(guest.Occupation))
+    {
+        tables["Table 2"].Add(guest);
+    }
+    else
+    {
+        tables["Table 1"].Add(guest);
+    }
+}
+
+foreach (var tableName in tables.Keys) {
+    Console.WriteLine(tableName);
+    foreach (var guest in tables[tableName]) {
+        Console.WriteLine($"  {guest.Name} ({guest.Occupation}) {guest.Bio}");
+    }
+}
+
+
+List<Guest> GetGuests()
+{
+    List<Guest> guests = new List<Guest>()
+    {
+        new Guest
+        {
+            Name = "Marilyn Monroe",
+            Occupation = "entertainer",
+            Bio = "(1926 - 1962) American actress, singer, model"
+        },
+        new Guest
+        {
+            Name = "Abraham Lincoln",
+            Occupation = "politician",
+            Bio = "(1809 - 1865) US President during American civil war"
+        },
+        new Guest
+        {
+            Name = "Martin Luther King",
+            Occupation = "activist",
+            Bio = "(1929 - 1968) American civil rights campaigner"
+        },
+        new Guest
+        {
+            Name = "Rosa Parks",
+            Occupation = "activist",
+            Bio = "(1913 - 2005) American civil rights activist"
+        },
+        new Guest
+        {
+            Name = "Peter Sellers",
+            Occupation = "entertainer",
+            Bio = "(1925 - 1980) British actor and comedian"
+        },
+        new Guest
+        {
+            Name = "Alan Turing",
+            Occupation = "computer scientist",
+            Bio = "(1912 - 1954) British computing pioneer, Turing machine, algorithms, cryptology, computer architecture, saved the world"
+        },
+        new Guest
+        {
+            Name = "Admiral Grace Hopper (1906–1992) – developed early compilers, FLOW-MATIC, COBOL",
+            Occupation = "computer scientist",
+            Bio = "(1906–1992) developed early compilers, FLOW-MATIC, COBOL"
+        },
+        new Guest
+        {
+            Name = "Indira Gandhi",
+            Occupation = "politician",
+            Bio = "(1917 - 1984) Prime Minister of India 1966 - 1977"
+        }
+    };
+
+    return guests;
+}
+
+public class Guest
+{
+    public string Name { get; set; }
+    public string Occupation { get; set; }
+    public string Bio { get; set; }
+}
