56e71ae7ea6e92662033080a9f51d52d810888dc
Gabrielle Schryver <gabbyschryver@gmail.com>
2023-05-23 12:05:11 -0400

    place names complete

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -5,8 +5,20 @@ const names = [
     "London", "The mall", "Ryman Auditorium", "The Great Wall of China"
 ];
 
-console.log("All Pace Names");
+console.log("All Place Names");
 
 for (const name of names) {
+  console.log(name);
+}
+
+// javascript solution 
+console.log("\n'The' Place Names (2)");
+const forTheNames = names.filter(name => name.startsWith("The"));
+forTheNames.forEach(name => console.log(name));
+
+// can also do
+console.log("\n'The' Place Names");
+theNames = names.filter(name => name.startsWith("The"));
+for (const name of theNames) {
   console.log(name);
 }
